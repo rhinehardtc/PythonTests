4645ec124504303872499b2b97a0397e82f8156d
rhinehardtc <rhinehardtc@gmail.com>
2020-12-11 00:33:56 -0600

    added some string manipulation notes

--- hello_world/hello.py
+++ hello_world/hello.py
@@ -3,4 +3,15 @@ msg = "hello world"
 
 print("peepee", msg.split())
 
+w = 2
+h = 2
+
+print((w+h) ** w)
+
+print(r'C:\some\name') #use r to escape special char after \
+
+print(3 * 'squeewoo weewoo ') #can multiple number of times a string is created
+
+s = 'This is a string, bro'
+print(len(s)) #len() is the Py length
 # %%
