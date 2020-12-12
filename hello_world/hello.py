1ea75dab2b8b60ea8fe95bc971aeff9ad7ce5465
rhinehardtc <rhinehardtc@gmail.com>
2020-12-11 18:06:48 -0600

    added function test

--- hello_world/hello.py
+++ hello_world/hello.py
@@ -14,4 +14,14 @@ print(3 * 'squeewoo weewoo ') #can multiple number of times a string is created
 
 s = 'This is a string, bro'
 print(len(s)) #len() is the Py length
+
+a, b = 0, 1
+while a < 30:
+    print(a)
+    a, b = b, a+b
+
+def tetrate(n):
+    print(n ** n)  
+
+tetrate(3)
 # %%
